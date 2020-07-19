30bfbfe1af67fe8898f5bf72fdc434518476ce4e
Chris Kay <ckay84@protonmail.com>
2020-07-19 13:33:45 -0400

    Add create action to users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,15 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(username: params[:username], password: params[:password])
+    binding.pry
+    if @user.save
+      session[:user_id] = @user.id 
+      redirect_to user_path(@user)
+    else
+      render :new
+    end 
+  end 
+
 end 
