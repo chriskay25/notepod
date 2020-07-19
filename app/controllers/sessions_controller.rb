0012869730675296b4805bd8349ec290b8f37e45
Chris Kay <ckay84@protonmail.com>
2020-07-19 16:49:50 -0400

    Add new and create actions to sessions_controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,4 +3,18 @@ class SessionsController < ApplicationController
   def home
   end 
 
+  def new
+  end 
+
+  def create
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id 
+      redirect_to user_path(user)
+    else
+      flash[:message] = "Username and/or password incorrect"
+      redirect_to login_path
+    end 
+  end 
+
 end 
