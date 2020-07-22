bbad87644d52827a73bb31fb447f37f0c87f9807
Chris Kay <ckay84@protonmail.com>
2020-07-22 12:59:17 -0400

    Add strong params, new and create actions, and new view/form for Podcast

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,2 +1,22 @@
 class PodcastsController < ApplicationController
+
+  def new
+    @podcast = Podcast.new
+  end 
+
+  def create
+    podcast = Podcast.new(podcast_params)
+    if podcast.save
+      redirect_to podcast_path(podcast)
+    else
+      render :new
+    end 
+  end 
+
+
+  private
+
+  def podcast_params
+    params.require(:podcast).permit(:name)
+  end   
 end
