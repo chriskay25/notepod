3b90eb0403b1cfb1414df9a9b190847f0a971464
Chris Kay <ckay84@protonmail.com>
2020-07-22 12:44:08 -0400

    Run resource generator to make Podcast model and related files

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'sessions#home'
-
+  
   get '/signup' => 'users#new'
   post '/signup' => 'users#create'
-
+  
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
-
+  
   resources :users, except: [:new, :create]
+  resources :podcasts
 end
  
