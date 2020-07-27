b2c259abad4d5d92a23089213f821598738116d6
Chris Kay <ckay84@protonmail.com>
2020-07-27 16:04:56 -0400

    Add relationships to Note model to Episode and User models

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -1,5 +1,7 @@
 class Episode < ApplicationRecord
   belongs_to  :podcast
+  has_many :notes
+  has_many :users, through: :notes
   validates :title, presence: true
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :subscriptions
   has_many :podcasts, through: :subscriptions
+  has_many :notes
+  has_many :episodes, through: :notes
   has_secure_password
   validates :username, presence: true, uniqueness: true
 end 
