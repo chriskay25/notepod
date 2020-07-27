984cb50dad57c8338af1acbe5570b854dc6eaccb
Chris Kay <ckay84@protonmail.com>
2020-07-27 16:05:55 -0400

    Add additional seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ user1 = User.create(username: "user1", password: "password1")
 user2 = User.create(username: "user2", password: "password2")
 user3 = User.create(username: "user3", password: "password3")
 user4 = User.create(username: "user4", password: "password4")
-user5 = User.create(username: "user5", password: "password5")
+user5 = User.create(username: "mariah", password: "sappington")
 user6 = User.create(username: "chris", password: "kay")
 
 pod1 = Podcast.create(name: "First Pod")
@@ -29,6 +29,8 @@ sub1 = Subscription.create(user_id: user6.id, podcast_id: pod1.id)
 sub2 = Subscription.create(user_id: user6.id, podcast_id: pod2.id)
 sub3 = Subscription.create(user_id: user6.id, podcast_id: pod3.id)
 
+sub4 = Subscription.create(user_id: user5.id, podcast_id: pod5.id)
+
 ep1 = Episode.create(title: "Episode 1", podcast_id: pod1.id)
 ep2 = Episode.create(title: "Episode 2", podcast_id: pod1.id)
 ep3 = Episode.create(title: "Episode 3", podcast_id: pod1.id)
@@ -42,3 +44,27 @@ ep9 = Episode.create(title: "Ep4", podcast_id: pod2.id)
 
 ep10 = Episode.create(title: "Number 1", podcast_id: pod3.id)
 ep11 = Episode.create(title: "Number 2", podcast_id: pod3.id)
+
+ep12 = Episode.create(title: "Uno", podcast_id: pod4.id)
+ep13 = Episode.create(title: "Dos", podcast_id: pod4.id)
+
+ep14 = Episode.create(title: "Ep. I", podcast_id: pod5.id)
+ep15 = Episode.create(title: "Ep. II", podcast_id: pod5.id)
+ep16 = Episode.create(title: "Ep. III", podcast_id: pod5.id)
+ep17 = Episode.create(title: "Ep. IV", podcast_id: pod5.id)
+
+note1 = Note.create(content: "First note's contents, blah blah blah.", user_id: user6.id, episode_id: ep1.id)
+note2 = Note.create(content: "Second note's contents, blah blah blah.", user_id: user6.id, episode_id: ep1.id)
+note3 = Note.create(content: "Third note's contents, blah blah blah.", user_id: user6.id, episode_id: ep2.id)
+note4 = Note.create(content: "Fourth note's contents, blah blah blah.", user_id: user6.id, episode_id: ep2.id)
+note5 = Note.create(content: "Fifth note's contents, blah blah blah.", user_id: user6.id, episode_id: ep3.id)
+note6 = Note.create(content: "Sixth note's contents, blah blah blah.", user_id: user6.id, episode_id: ep3.id)
+note7 = Note.create(content: "Seventh note's contents, blah blah blah.", user_id: user6.id, episode_id: ep3.id)
+note8 = Note.create(content: "Eighth note's contents, blah blah blah.", user_id: user6.id, episode_id: ep4.id)
+
+note9 = Note.create(content: "Ninth note's contents, blah blah blah.", user_id: user5.id, episode_id: ep14.id)
+note10 = Note.create(content: "Tenth note's contents, blah blah blah.", user_id: user5.id, episode_id: ep15.id)
+note11 = Note.create(content: "Eleventh note's contents, blah blah blah.", user_id: user5.id, episode_id: ep16.id)
+note12 = Note.create(content: "Twelfth note's contents, blah blah blah.", user_id: user5.id, episode_id: ep17.id)
+
+
