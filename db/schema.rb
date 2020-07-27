fde79f7cb1187fd3a01193ec22d2955d6e50e421
Chris Kay <ckay84@protonmail.com>
2020-07-27 13:20:53 -0400

    Add and run migration to create Note model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_26_182727) do
+ActiveRecord::Schema.define(version: 2020_07_26_200856) do
 
   create_table "episodes", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,16 @@ ActiveRecord::Schema.define(version: 2020_07_26_182727) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["podcast_id"], name: "index_episodes_on_podcast_id"
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.integer "episode_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["episode_id"], name: "index_notes_on_episode_id"
+    t.index ["user_id"], name: "index_notes_on_user_id"
   end
 
   create_table "podcasts", force: :cascade do |t|
@@ -41,6 +51,8 @@ ActiveRecord::Schema.define(version: 2020_07_26_182727) do
   end
 
   add_foreign_key "episodes", "podcasts"
+  add_foreign_key "notes", "episodes"
+  add_foreign_key "notes", "users"
   add_foreign_key "subscriptions", "podcasts"
   add_foreign_key "subscriptions", "users"
 end
