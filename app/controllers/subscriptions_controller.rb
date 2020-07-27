873cb0681c250b7b386c4897e587cb206f72905a
Chris Kay <ckay84@protonmail.com>
2020-07-27 13:16:07 -0400

    Add subscriptions#create action and strong params

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -4,4 +4,22 @@ class SubscriptionsController < ApplicationController
     @subscription = Subscription.new
   end
 
+  def create
+    binding.pry
+    @sub = current_user.subscriptions.build(user_id: current_user.id, podcast_id: params[:podcast_id])
+    if @sub.save
+      redirect_to user_path(current_user)
+    else
+      flash[:message] = "Something went wrong."
+      render :'podcasts/index'
+    end 
+  end
+
+
+  private
+
+  def subscription_params
+    params.require(:subscription).permit(:podcast_id)
+  end 
+
 end 
