403d8d71e513ebd81081cb9ae76f618a2e65f016
Chris Kay <ckay84@protonmail.com>
2020-07-28 14:28:56 -0400

    In notes#new & create actions, check for nested params, build note accordingly

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,10 +1,25 @@
 class NotesController < ApplicationController
 
   def new
-    @note = Note.new
+    if params[:episode_id] && episode = Episode.find_by(id: params[:episode_id])
+      @note = episode.notes.build
+    else
+      @note = Note.new
+    end
   end
 
   def create
+    if params[:episode_id] && episode = Episode.find_by(id: params[:episode_id])
+      @note = episode.notes.build(notes_params)
+    else
+      @note = Note.new(notes_params)
+    end
+
+    if @note.save
+      redirect_to note_path(@note)
+    else
+      render :new
+    end 
   end 
 
   private
