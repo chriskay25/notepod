670e84f835d3317ea0b19f58c306b1d59d55834b
Chris Kay <ckay84@protonmail.com>
2020-07-28 16:53:22 -0400

    Add scope method to episode model to get most noted episode

--- app/models/episode.rb
+++ app/models/episode.rb
@@ -3,5 +3,11 @@ class Episode < ApplicationRecord
   has_many :notes
   has_many :users, through: :notes
   validates :title, presence: true
+
+  scope :most_noted, -> { order(note_count: :desc).limit(1) }
+
+  def note_count
+    notes.size
+  end 
   
 end
