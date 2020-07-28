dc8d63b4a634ad93c60537ac869b09ff5ed2a168
Chris Kay <ckay84@protonmail.com>
2020-07-28 13:15:04 -0400

    Edit podcasts#create action to check for nested form & subscribe if present

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -9,11 +9,19 @@ class PodcastsController < ApplicationController
   end 
 
   def create
-    podcast = Podcast.new(podcast_params)
-    if podcast.save
-      redirect_to podcast_path(podcast)
+    if params[:user_id] && user = User.find_by(id: params[:user_id])
+      podcast = Podcast.new(podcast_params)
+      if podcast.save
+        podcast.subscriptions.create(user_id: user.id, podcast_id: podcast.id)
+        redirect_to user_path(user)
+      end
     else
-      render :new
+      podcast = Podcast.new(podcast_params)
+      if podcast.save
+        redirect_to podcast_path(podcast)
+      else
+        render :new
+      end 
     end 
   end 
 
