bb0c7ae202ae681ba6e718ccb0e82eedf36ea7ec
Chris Kay <ckay84@protonmail.com>
2020-07-28 16:12:36 -0400

    Creation of note only happens through nested routes, simplify controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,19 +1,18 @@
 class NotesController < ApplicationController
 
   def new
-    if params[:episode_id] && episode = Episode.find_by(id: params[:episode_id])
-      @note = episode.notes.build
-    else
-      @note = Note.new
+    if params[:episode_id] && @episode = Episode.find_by(id: params[:episode_id])
+      @note = @episode.notes.build
     end
   end
 
   def create
-    if params[:episode_id] && episode = Episode.find_by(id: params[:episode_id])
-      @note = episode.notes.build(note_params)
-    else
-      @note = Note.new(note_params)
-    end
+    # if params[:episode_id] && episode = Episode.find_by(id: params[:episode_id])
+      # @note = episode.notes.build(note_params)
+    @note = Note.new(note_params)
+    # else
+    #   @note = Note.new(note_params)
+    # end
 
     if @note.save
       redirect_to note_path(@note)
@@ -23,13 +22,13 @@ class NotesController < ApplicationController
   end 
 
   def show
-    @note = Note.find_by(params[:id])
+    @note = Note.find_by(id: params[:id])
   end 
 
   private
 
   def note_params
-    params.require(:note).permit(:content)
+    params.require(:note).permit(:content, :user_id, :episode_id)
   end
 
 end 
