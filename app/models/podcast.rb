3bfddd5a9a8cf475eb846cb7adbdc81a6b9bf682
Chris Kay <ckay84@protonmail.com>
2020-08-14 13:11:43 -0400

    Add all_sorted scope method to Podcast model to alph sort index

--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -3,4 +3,6 @@ class Podcast < ApplicationRecord
   has_many :subscriptions
   has_many :users, through: :subscriptions
   validates :name, presence: true, uniqueness: true
+
+  scope :all_sorted, -> { order(name: :asc) }
 end
