9f4721888c01ea9c57c190175a28d9bfe1948824
Chris Kay <ckay84@protonmail.com>
2020-08-17 13:09:12 -0400

    Edit notes#create action to build note from episode

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -7,17 +7,13 @@ class NotesController < ApplicationController
   end
 
   def create
-    # if params[:episode_id] && episode = Episode.find_by(id: params[:episode_id])
-      # @note = episode.notes.build(note_params)
-    @note = Note.new(note_params)
-    # else
-    #   @note = Note.new(note_params)
-    # end
-
-    if @note.save
-      redirect_to note_path(@note)
-    else
-      render :new
+    if params[:episode_id] && episode = Episode.find_by(id: params[:episode_id])
+      @note = episode.notes.build(note_params)
+      if @note.save
+        redirect_to note_path(@note)
+      else
+        render :new
+      end 
     end 
   end 
 
